c9cbd1320ba8733847186bca438a76ff13f0d8d4
aalhojory <aalhojory@deakin.edu.au>
2021-09-14 01:47:30 +1000

    Add delete function for category.js

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -3,27 +3,36 @@ const router = express.Router();
 
 const catController = require("../controllers/category");
 
-router.post("/", (req, res) => {
-     if (req.isAuthenticated())
-          catController.createCategory(req, res)
-     else
-          res.redirect('/auth/login')
-}),
-router.post("/update/", (req, res) => {
-          if (req.isAuthenticated())
-               catController.updateCategory(req, res)
-          else
-               res.redirect('/auth/login')
-}),
+router.post("/",(req,res) =>{
+    if(req.isAuthenticated())
+         catController.createCategory(req,res)
+    else
+        res.redirect('/auth/login')
+     }),
 
 
-     router.get("/", (req, res) => {
-          if (req.isAuthenticated())
-               catController.getCategory(req, res)
+router.post("/update/",(req,res) =>{
+          if(req.isAuthenticated())
+               catController.updateCategory(req,res)
           else
-               res.redirect('/auth/login')
+              res.redirect('/auth/login')
+           }),
+
+router.post("/delete/",(req,res) =>{
+               if(req.isAuthenticated())
+                    catController.deleteCategory(req,res)
+               else
+                   res.redirect('/auth/login')
+                }),
+
+
+router.get("/",(req,res) =>{
+    if(req.isAuthenticated())
+         catController.getCategory(req,res)
+    else
+        res.redirect('/auth/login')
      }),
 
 
 
-     module.exports = router;
+module.exports = router;
