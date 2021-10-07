e8d13987a6d484490b813798dc00825424360699
Faisalalharthi <88130578+Faisalalharthi@users.noreply.github.com>
2021-10-08 01:34:49 +1100

    add function to send email notification when add new policy

--- controllers/policy.js
+++ controllers/policy.js
@@ -1,9 +1,14 @@
 const url = require('url')
 const path = require('path')
 const policy = require('../models/policy')
+const UserDetail = require('../models/UserDetail')
 const category = require('../models/category')
+const nodemailer = require("nodemailer");
 const socket = require('../app')
+var email = "cspmsgroup@gmail.com";
+var password = "Abdu@1405";
 
+//console.log("email",email,"password",password)
 //// this function is used to create send file from database
 const getFile = async (req, res) => {
   try {
@@ -49,6 +54,49 @@ const createPolicy = async (req, res) => {
     let cat = await category.find().lean()
     let plc = await policy.find().lean()
     let a = await socket.sockets.emit('message', { type: 'policy'})
+    var email = "cspmsgroup@gmail.com";
+    var password = "Abdu@1405";
+    console.log("email",email,"password",password)
+    UserDetail.find({'role':'Manager'}, function(err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        var transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: email,
+            pass: password,
+          },
+        });
+        for (var i = 0; i < result.length; i++) { 
+          console.log(result[i]['email']);
+          var mailOptions = {
+            from: email,
+            to: result[i]['email'],
+            subject: "CSPMS New Policy Added",
+            text: "Visit out Site to check New Policies which are added now. Thanks " ,
+          };
+    
+          transporter
+            .sendMail(mailOptions)
+            .then((result) => {
+              console.log("done")
+            })
+            .catch((error) => {
+              console.error(" email error",error);
+    
+              return res.status(500).json({
+                success: false,
+                message: error.message,
+              });
+            });
+        
+        
+        }
+        //console.log(result);
+      }
+    });
+   
     res.render('policy.ejs', {
       data: {
         success: 'Policy Uploaded Successfully',
